eb7915c54261bcbfd83f0cd562c4cc6bd3e8eb76
unknown <5131523+nickgogan@users.noreply.github.com>
2022-05-29 12:41:29 -0400

    Move Dto and subclasses to new Models package.

--- src/main/java/personal/ng/Models/Dto.java
+++ src/main/java/personal/ng/Models/Dto.java
@@ -1,4 +1,4 @@
-package personal.ng;
+package personal.ng.Models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,113 +11,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-@JsonInclude(JsonInclude.Include.ALWAYS)
-@JsonPropertyOrder({
-        "body",
-        "email",
-        "author"
-})
-@Generated("jsonschema2pojo")
-class Comment {
-    @JsonProperty("body")
-    public String body;
-    @JsonProperty("email")
-    public String email;
-    @JsonProperty("author")
-    public String author;
-    @JsonIgnore
-    public Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    /**
-     * No args constructor for use in serialization
-     *
-     */
-    public Comment() {
-    }
-
-    /**
-     *
-     * @param author
-     * @param body
-     * @param email
-     */
-    public Comment(String body, String email, String author) {
-        super();
-        this.body = body;
-        this.email = email;
-        this.author = author;
-    }
-
-    @JsonProperty("body")
-    public String getBody() {
-        return body;
-    }
-
-    @JsonProperty("body")
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    @JsonProperty("email")
-    public String getEmail() {
-        return email;
-    }
-
-    @JsonProperty("email")
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @JsonProperty("author")
-    public String getAuthor() {
-        return author;
-    }
-
-    @JsonProperty("author")
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Comment.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("body");
-        sb.append('=');
-        sb.append(((this.body == null)?"<null>":this.body));
-        sb.append(',');
-        sb.append("email");
-        sb.append('=');
-        sb.append(((this.email == null)?"<null>":this.email));
-        sb.append(',');
-        sb.append("author");
-        sb.append('=');
-        sb.append(((this.author == null)?"<null>":this.author));
-        sb.append(',');
-        sb.append("additionalProperties");
-        sb.append('=');
-        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
-    }
-
-}
 
 @JsonInclude(JsonInclude.Include.ALWAYS)
 @JsonPropertyOrder({
