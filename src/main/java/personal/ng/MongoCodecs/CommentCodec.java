328748138585e3ff6dad3adac58b694d5f4e8fe8
unknown <5131523+nickgogan@users.noreply.github.com>
2022-05-29 12:42:04 -0400

    Add new MongoCodec package with classes for each model in Models.

--- src/main/java/personal/ng/MongoCodecs/CommentCodec.java
+++ src/main/java/personal/ng/MongoCodecs/CommentCodec.java
@@ -1,2 +1,26 @@
-package personal.ng.MongoCodecs;public class CommentCodec {
+package personal.ng.MongoCodecs;
+
+import org.bson.BsonReader;
+import org.bson.BsonWriter;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+import personal.ng.Models.Comment;
+
+public class CommentCodec implements Codec<Comment> {
+
+    @Override
+    public Comment decode(BsonReader bsonReader, DecoderContext decoderContext) {
+        return null;
+    }
+
+    @Override
+    public void encode(BsonWriter bsonWriter, Comment comment, EncoderContext encoderContext) {
+
+    }
+
+    @Override
+    public Class<Comment> getEncoderClass() {
+        return null;
+    }
 }
--- src/main/java/personal/ng/MongoCodecs/DtoCodec.java
+++ src/main/java/personal/ng/MongoCodecs/DtoCodec.java
@@ -1,2 +1,26 @@
-package personal.ng.MongoCodecs;public class DtoCodec {
+package personal.ng.MongoCodecs;
+
+import org.bson.BsonReader;
+import org.bson.BsonWriter;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+import personal.ng.Models.Dto;
+
+public class DtoCodec implements Codec<Dto> {
+
+    @Override
+    public Dto decode(BsonReader bsonReader, DecoderContext decoderContext) {
+        return null;
+    }
+
+    @Override
+    public void encode(BsonWriter bsonWriter, Dto dto, EncoderContext encoderContext) {
+
+    }
+
+    @Override
+    public Class<Dto> getEncoderClass() {
+        return null;
+    }
 }
